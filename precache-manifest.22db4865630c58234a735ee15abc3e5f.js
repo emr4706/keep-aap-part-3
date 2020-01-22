dd98f40a30e875d970ae818df8a91b24a8be2932
emr4706 <emr4706@gmail.com>
2020-01-22 22:01:44 +0100

    Updates

---
+++ precache-manifest.22db4865630c58234a735ee15abc3e5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2577a3970cb0ac721d9672130663e5",
+    "url": "/keep-aap-part-3/index.html"
+  },
+  {
+    "revision": "3d61aadf3da37aaa49a8",
+    "url": "/keep-aap-part-3/static/css/main.57057ee8.chunk.css"
+  },
+  {
+    "revision": "ca022b273b035f501bb4",
+    "url": "/keep-aap-part-3/static/js/2.721cc876.chunk.js"
+  },
+  {
+    "revision": "49309882d0acd110106c7be3de4100a8",
+    "url": "/keep-aap-part-3/static/js/2.721cc876.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d61aadf3da37aaa49a8",
+    "url": "/keep-aap-part-3/static/js/main.5bdcf28f.chunk.js"
+  },
+  {
+    "revision": "17d9843f4a1eedd2b96a",
+    "url": "/keep-aap-part-3/static/js/runtime-main.b791a367.js"
+  }
+]);
